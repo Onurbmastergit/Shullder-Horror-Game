0c5491ac81e23322a58f4607ff1e1444b27a6a33
Onurbmastergit <142446897+Onurbmastergit@users.noreply.github.com>
2023-11-06 17:08:37 -0300

    second version

--- Assets/Script/Cellphone.cs
+++ Assets/Script/Cellphone.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Cellphone : MonoBehaviour
@@ -7,9 +8,17 @@ public class Cellphone : MonoBehaviour
 {
       public GameObject cam;
      bool on = true;
+     public float zoom_max = 30;
+      public float zoom_minum = 50;
 
     void Update()
     {
+      if(Input.GetKeyDown(KeyCode.Q)){
+        cam.GetComponent<Camera>().fieldOfView = zoom_max;
+      }
+      if(Input.GetKeyDown(KeyCode.Z)){
+        cam.GetComponent<Camera>().fieldOfView = zoom_minum;
+      }
 
 
      if(Input.GetKeyDown(KeyCode.E)){
