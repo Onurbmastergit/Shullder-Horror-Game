d8aec5c5f90d77aa03f9f1f3f184e1941039ba8b
Onurbmastergit <142446897+Onurbmastergit@users.noreply.github.com>
2023-11-08 02:45:09 -0300

    5rd version
    Monster girl remove animation

--- Assets/Monster/Script/Script/Seguir_Inimigo.cs
+++ Assets/Monster/Script/Script/Seguir_Inimigo.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Seguir_Inimigo : MonoBehaviour
 {
     Rigidbody rb;
-    private Animator animInimigo;
     private UnityEngine.AI.NavMeshAgent navMesh;
     private GameObject player;
     public float velocidadeInimigo;
@@ -15,24 +14,20 @@ public class Seguir_Inimigo : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>(); // resumir GetCompon...
         rb.freezeRotation = true; // travar rotacao de personagem ao inserir for�a
-
-        animInimigo = GetComponent<Animator>();
-        navMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        player = GameObject.FindGameObjectWithTag("Player");
-        navMesh.speed = velocidadeInimigo;
-
     }
 
 
     void Update()
     {
-        navMesh.destination = player.transform.position;
-        animInimigo.SetBool("walk", true);
 
+         navMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMesh.speed = velocidadeInimigo;
+
+        navMesh.destination = player.transform.position;
         if(Vector3.Distance(transform.position, player.transform.position) < 1.5f);
         {
             navMesh.speed = 0;
-            animInimigo.SetBool("atack", true);
             StartCoroutine("ataque");
         }
 
@@ -41,7 +36,6 @@ public class Seguir_Inimigo : MonoBehaviour
     IEnumerator ataque()
     {
         yield return new WaitForSeconds(2.8f);
-        animInimigo.SetBool("atack", false);
         navMesh.speed = velocidadeInimigo;
     }
 
